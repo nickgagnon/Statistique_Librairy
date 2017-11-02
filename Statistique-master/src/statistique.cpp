5d1862544a3ffe2a319485fda5db3db6a48bb339
Nicolas Gagnon <32691913+nickgagnon@users.noreply.github.com>
2017-11-02 16:40:13 -0400

    Add files via upload
    Simple function like mean, maximum and minimum

---
+++ Statistique-master/src/statistique.cpp
@@ -0,0 +1,51 @@
+#include "statistique.h"
+
+unsigned int Max(unsigned int *Table, unsigned int Amount) {
+  unsigned int  Ans = 0;
+  for (unsigned int  i = 0; i < Amount; i++) {
+    if (*(Table + i) > Ans) {
+      Ans = *(Table + i);
+    }
+  }
+  return Ans;
+}
+
+unsigned int  Min(unsigned int *Table, unsigned int Amount) {
+  unsigned int Ans = 1024;
+  for (unsigned int i = 0; i < Amount; i++) {
+    if (*(Table + i) < Ans) {
+      Ans = *(Table + i);
+    }
+  }
+  return Ans;
+}
+
+unsigned int Moy(unsigned int *Table, unsigned int Amount) {
+  unsigned int Total = 0;
+  for (unsigned int  i = 0; i < Amount; i++) {
+    Total += *(Table + i);
+  }
+  float moyenne = Total * 1.0 / Amount;
+  return moyenne;
+}
+
+void Stat(unsigned int *Table, unsigned int Amount, unsigned int *Max, unsigned int *Min, unsigned int *Moy ) {
+  unsigned int Maximum = 0;
+  unsigned int Minimum = 1024;
+  long Total = 0;
+
+  for (unsigned int i = 0; i < Amount; i++) {
+    Total += *(Table + i);
+    if (*(Table + i) > Maximum) { //Max
+      Maximum = *(Table + i);
+    }
+    if (*(Table + i) < Minimum) { //Min
+      Minimum = *(Table + i);
+    }
+  }
+  int moyenne = Total / Amount;
+  *Max = Maximum;
+  *Min = Minimum;
+  *Moy = moyenne;
+}
+
---
+++ Statistique-master/src/statistique.h
@@ -0,0 +1,8 @@
+unsigned int Max(unsigned int *Table, unsigned int Amount);
+
+unsigned int Min(unsigned int *Table, unsigned int Amount);
+
+unsigned int Moy(unsigned int *Table, unsigned int Amount);
+
+void Stat(unsigned int *Table, unsigned int Amount, unsigned int *Max, unsigned int *Min, unsigned int *Moy );
+
